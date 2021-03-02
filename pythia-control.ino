daa172feca35932d00a139bb69ed393f998a988a
lmanhes <narraflow@gmail.com>
2021-03-02 19:55:47 +0100

    Update pythia-control.ino

--- pythia-control.ino
+++ pythia-control.ino
@@ -1,9 +1,14 @@
 #include <Wire.h>
-#include "motor_control.cpp"
+//#include "motor_control.cpp"
+#include <VarSpeedServo.h> 
 
 
-Motor pan_servo(50, 130, 90);
-Motor tilt_servo(120, 180, 170);
+
+//Motor pan_servo(50, 130, 90);
+//Motor tilt_servo(120, 180, 170);
+
+VarSpeedServo pan_servo;
+VarSpeedServo tilt_servo;
 
 
 void setup() {
@@ -13,14 +18,14 @@ void setup() {
   Wire.onReceive(receiveData);  // register receive event
   Wire.onRequest(sendData);     // register sending event
   
-  pan_servo.Attach(9);
-  tilt_servo.Attach(10);
+  pan_servo.attach(9);
+  tilt_servo.attach(10);
 }
 
 
 void loop() {
-  pan_servo.Update();
-  tilt_servo.Update();
+  //pan_servo.Update();
+  //tilt_servo.Update();
 }
 
 
@@ -42,13 +47,15 @@ void receiveData(int howMany) {
       Serial.println("");
       if (Buffer[0] == 0)
       {
-        pan_servo.UpdatePosition(Buffer[1]);
-        pan_servo.UpdateSpeed(Buffer[2]);
+        //pan_servo.UpdatePosition(Buffer[1]);
+        //pan_servo.UpdateSpeed(Buffer[2]);
+        pan_servo.write(Buffer[1], Buffer[2], false);
       }
       else 
       {
-        tilt_servo.UpdatePosition(Buffer[1]);
-        tilt_servo.UpdateSpeed(Buffer[2]);
+        //tilt_servo.UpdatePosition(Buffer[1]);
+        //tilt_servo.UpdateSpeed(Buffer[2]);
+        tilt_servo.write(Buffer[1], Buffer[2], false);
       }
     }
   }
@@ -57,9 +64,13 @@ void receiveData(int howMany) {
 
 void sendData() {
   uint8_t Buffer[2];
-  Buffer[0] = pan_servo.GetSpeed();
-  Buffer[1] = pan_servo.GetPosition();
-  Buffer[2] = tilt_servo.GetSpeed();
-  Buffer[3] = tilt_servo.GetPosition();
+  //Buffer[0] = pan_servo.GetSpeed();
+  //Buffer[1] = pan_servo.GetPosition();
+  //Buffer[2] = tilt_servo.GetSpeed();
+  //Buffer[3] = tilt_servo.GetPosition();
+  Buffer[0] = 0;
+  Buffer[1] = 0;
+  Buffer[2] = 0;
+  Buffer[3] = 0;
   Wire.write(Buffer, 4); 
 }
